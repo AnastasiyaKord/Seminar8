44d0d45bada3929e0bd6d5643df858c8aa927148
Anastasiya <vipstop2303@mail.ru>
2023-06-09 14:05:26 +0300

    Задача53

--- Задача53/Program.cs
+++ Задача53/Program.cs
@@ -1,36 +1,89 @@
 ﻿// Задача 53: Задайте двумерный массив. 
 // Напишите программу, которая поменяет 
 // местами первую и последнюю строку массива.
-class Program
+// class Program
+// {
+//     static void Main(string[] args)
+//     {
+//        int [,] matrix = GetRandomMatrix(rows: 5, columns: 5, leftBorder: 0, rightBorder: 10);
+//        PrintMatrix(matrix);
+//        Print("n\n");
+
+//        matrix = SwapMatrixLastFirst(matrix);
+//        PrintMatrix(matrix);
+//     }
+// }
+
+// public static int[,] SwapMatrixLastFirst(int[,] matrix)
+// {
+//     for (int i = 0; i < matrix.GetLength(dimension: 1); i++)
+//     {
+//         int temp = matrix[0; i];
+//         matrix[0, i] = matrix[matrix.GetLength(dimension: 0) - 1, i];
+//         matrix[matrix.GetLength(dimension: 0) - 1, i] = temp;
+//     }
+//     return matrix;
+// }
+
+// public static void WorkFormater(String articul, String brand, String price)
+// {
+//     Console.BackgroundColor = ConsoleColor.Gray;
+//     Console.ForegroundColor = СonsoleColor.White;
+//     int princeint = Int32.Parse(price);
+//     princeint($ <offer sku=\"{articul}\">\n");
+
+//     // и т.д. дальше решение задачи не показали:)
+// }
+
+
+int ReadNumber(string messageToUser)
 {
-    static void Main(string[] args)
+    Console.WriteLine(messageToUser);
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+int[,] GetRandomMatrix (int rows, int columns, int leftBorder = 0, int rightBorder = 10)
+{   
+    int[,] matrix = new int [rows, columns];
+       for (int i = 0; i < matrix.GetLength(0); i++)
+       {
+           for (int j = 0; j < matrix.GetLength(1); j++)
+           {
+               matrix[i, j] = Random.Shared.Next(leftBorder,rightBorder + 1);
+           }
+       }
+       return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+     for (int i = 0; i < matrix.GetLength(0); i++)
+       {
+           for (int j = 0; j < matrix.GetLength(1); j++)
+           {
+               Console.Write(matrix[i, j] + " "); 
+           }
+           Console.WriteLine();
+       }
+}
+
+void SwapBorderLines(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(1); i++)
     {
-       int [,] matrix = GetRandomMatrix(rows: 5, columns: 5, leftBorder: 0, rightBorder: 10);
-       PrintMatrix(matrix);
-       Print("n\n");
-
-       matrix = SwapMatrixLastFirst(matrix);
-       PrintMatrix(matrix);
+        int temp = matrix[0, i];
+        matrix[0, i] = matrix[matrix.GetLength(0) - 1, i];
+        matrix[matrix.GetLength(0) - 1, i] = temp;
     }
-}
 
-public static int[,] SwapMatrixLastFirst(int[,] matrix)
-{
-    for (int i = 0; i < matrix.GetLength(dimension: 1); i++)
-    {
-        int temp = matrix[0; i];
-        matrix[0, i] = matrix[matrix.GetLength(dimension: 0) - 1, i];
-        matrix[matrix.GetLength(dimension: 0) - 1, i] = temp;
-    }
-    return matrix;
 }
+int m = ReadNumber("Введите колличество строк:");
+int n = ReadNumber("Введите колличество столбцов:");
+int [,] myMatrix = GetRandomMatrix(m, n);
+PrintMatrix(myMatrix);
+SwapBorderLines(myMatrix);
+Console.WriteLine("------------------");
+PrintMatrix(myMatrix);
 
-public static void WorkFormater(String articul, String brand, String price)
-{
-    Console.BackgroundColor = ConsoleColor.Gray;
-    Console.ForegroundColor = СonsoleColor.White;
-    int princeint = Int32.Parse(price);
-    princeint($ <offer sku=\"{articul}\">\n");
 
-    // и т.д. дальше решение задачи не показали:)
-}
---
+++ Задача55/Program.cs
@@ -0,0 +1,74 @@
+﻿// Задача 55: Задайте двумерный массив. 
+// Напишите программу, которая заменяет 
+// строки на столбцы. В случае, если это 
+// невозможно, программа должна вывести 
+// сообщение для пользователя.
+
+// class Program
+// {
+//     static void Main(string[] args)
+//     {
+//         int[,] matrix = GetRandomMatrixColor(rows: 5, columns: 5, leftBorder: 0, rightBorder: 10);
+//         PrintMatrix(matrix);
+//         Print ("\n\n\n");
+//         matrix = RotateMatrix(matrix);
+//         PrintMatrix(matrix);
+//     }
+
+
+// public static int[,] SwapMatrixLastFirst(int[,] matrix)
+// {
+//     for (int i = 0; i < matrix.GetLength(dimension: 1); i++)
+//     {
+//         int temp = matrix[0; i];
+//         matrix[0, i] = matrix[matrix.GetLength(dimension: 0) - 1, i];
+//         matrix[matrix.GetLength(dimension: 0) - 1, i] = temp;
+//     }
+//     return matrix;
+// }
+
+// public static int[,] RotateMatrix(int[,] matrix)
+// {
+//     int[,] newMatrix = new int[matrix.GetLength(dimension: 1), matrix.GetLength(dimension: 0)];
+//     for (int i = 0; i < matrix.GetLength(dimension: 0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(dimension: 1); j++)
+//         {
+//             newMatrix[j, i] = matrix[i, j];
+//         }
+//     }
+//     return newMatrix;
+// }
+// public static void WorkFormater(String articul, String brand, String price)
+// {
+//     Console.BackgroundColor = ConsoleColor.Gray;
+//     Console.ForegroundColor = СonsoleColor.White;
+//     int princeint = Int32.Parse(price);
+//     princeint($ <offer sku=\"{articul}\">\n");
+// }
+
+// public static void PrintMatrixColor(int[,] matrix)
+// {
+//     for (int i = 0; i < matrix.GetLength(dimension: 0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(dimension: 1); j++)
+//         {
+//             if(matrix[i, j] > 7)
+//             {
+//                 Console.BackgroundColor = ConsoleColor.Red;
+//                 Console.Write(matrix[i, j] + " ");
+//             }
+//             Console.Write(matrix[i, j] + " ");
+//         }
+//         Console.WriteLine();
+//     }    
+// }
+
+// public static void PrintMatrixColor(double[,] matrix)
+// {
+//     for (int i = 0; i < matrix.GetLength(dimension: 0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(dimension: 1); j++)
+//         {
+// }
+// }
